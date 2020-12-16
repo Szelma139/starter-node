2849409eed6a03f17a73ac34256a8db31c22f4dd
Szelma139 <raduniek1@gmail.com>
2020-12-16 18:30:53 +0100

    testing li

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import '@styles/globals.css'
+import '@styles/skeleton.css'
+import '@styles/normalize.css';
 
 function Application({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
 
 export default function Home() {
   return (
@@ -15,9 +15,34 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+        <div className="container">
+
+          <div className="row">
+            <div className="three columns">
+              <a className="button button-primary" href="#">
+                Anchor button
+              </a>
+            </div>
+            <div className="three columns">
+              <a className="button button-primary" href="#">
+                Anchor button
+              </a>
+            </div>
+            <div className="three columns">
+              <a className="button button-primary" href="#">
+                Anchor button
+              </a>
+            </div>
+            <div className="three columns">
+              <a className="button button-primary" href="#">
+                Anchor button
+              </a>
+            </div>
+          </div>
+        </div>
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
