938b562142b28599edee9cb1c0b9a14de533057e
Szelma139 <raduniek1@gmail.com>
2020-12-17 18:11:50 +0100

    styled components

--- pages/index.js
+++ pages/index.js
@@ -3,25 +3,36 @@ import Header from "@components/Header";
 import Footer from "@components/Footer";
 import { Card } from "@components/Card";
 
+import styled from "styled-components";
 
 export default function Home() {
+  const Cards = styled.div`
+    width: 100%;
+    display: flex;
+  `;
+
+  const CardContainer = styled.div`
+ height:300px;
+    width:25%;
+  `;
+
   return (
     <div>
-<div className="cards">
-  <div className="card">
-    <Card/>
-  </div>
-  <div className="card">
-    <Card/>
-  </div>
-  <div className="card">
-    <Card/>
-  </div>
-  <div className="card">
-    <Card/>
-  </div>
-  </div>
-
+      <Cards>
+       <CardContainer>
+       <Card />
+       </CardContainer>
+       <CardContainer>
+       <Card />
+       </CardContainer>
+       <CardContainer>
+       <Card />
+       </CardContainer>
+       <CardContainer>
+       <Card />
+       </CardContainer>
+       
+      </Cards>
       <Footer />
     </div>
   );
